db996f9f5a0a69c0ffbcdf0948927393e9de0a0b
cuong.nguyenvan <cuong.codecomplete@gmail.com>
2019-11-21 16:21:10 +0700

    feature/admin/supplier

---
+++ src/test/java/test/TestMath.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestMath {
+    public static void main(String[] args) {
+       double a = Math.ceil(11/5);
+        System.out.println(a);
+    }
+}
